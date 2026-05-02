71764943e8b68da2c9a658dc86804e56234fb443
Maria Laura Navarro <24909847@itbeltran.com.ar>
2026-05-02 06:54:42 -0300

    guardo antes de cargar los destinos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Destiny } from './pages/destiny/destiny';
+import { Contact } from './pages/contact/contact';
+import { Notfound } from './pages/notfound/notfound';
+
+export const routes: Routes = [
+ {path: '', component:Home},
+ {path: 'Conocenos', component:About},
+ {path: 'Destinos', component:Destiny},
+ {path: 'Contactanos', component:Contact},
+ {path: '**', component:Notfound}
+
+ 
+
+];
